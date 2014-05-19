4d1810707b8ae7c4315ecdbefe48bcfa0f482641
Dave Lafferty <claffer2@naz.edu>
2014-05-19 11:13:09 -0400

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is a lightweight api for some features I think should already exist.
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+    name='ww_api',
+    version='0.1.1',
+    author='Dave Lafferty',
+    author_email='info@dave-lafferty.com',
+    packages=['ww_api',],
+    scripts=[],
+    url='https://github.com/ceko/ww_api',
+    license='LICENSE.txt',
+    description='Third-party api for wizard wars',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
